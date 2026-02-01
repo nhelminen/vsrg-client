245d02b5a013a07811296a54add27d61f25a3ad3
ItzFireable <fireableplayz@gmail.com>
2026-02-01 17:02:51 +0200

    process the queue on destroy

--- src/core/debug.cpp
+++ src/core/debug.cpp
@@ -61,6 +61,26 @@ namespace vsrg
             }
         }
 
+        while (!logQueue.empty())
+        {
+            std::string message = logQueue.front();
+            logQueue.pop();
+
+            if (message.find("[ERROR]") != std::string::npos)
+            {
+                std::cerr << message << std::endl;
+            }
+            else
+            {
+                std::cout << message << std::endl;
+            }
+
+            if (saveToFile && logFile.is_open())
+            {
+                logFile << message << std::endl;
+            }
+        }
+
         if (logFile.is_open())
         {
             logFile.close();
